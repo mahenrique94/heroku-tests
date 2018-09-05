77efcc2ab3cd3a973b2628f59bc6d4b1b3fcb699
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2018-09-04 21:07:28 -0300

    Changing API port

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ server.get('/', (req, res, next) => {
     next()
 })
 
-server.listen(80, 'localhost', () => {
-    console.log('Server running, to access it go to http://localhost:80')
+server.listen(3030, 'localhost', () => {
+    console.log('Server running, to access it go to http://localhost:3030')
     console.log('Use CTRL+C to stop it')
 })
